39fff80ecd3cded17b7f38ea092c6c64a82b5069
Jonas <jonas.otten.00@gmail.com>
2025-02-20 12:23:19 +0100

    Add configuration management with dataclass

--- ml-experiments/project/src/image_classification/main.py
+++ ml-experiments/project/src/image_classification/main.py
@@ -1,6 +1,10 @@
+from pprint import pformat
+import yaml
+from dataclasses import dataclass
 import logging
+import sys
 import torch
-from torch import nn, optim
+from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
@@ -11,9 +15,26 @@ from image_classification.conv_net import ConvModel
 logger = logging.getLogger(__name__)
 
 
+@dataclass(frozen=True)
+class TrainingConfig:
+    epochs: int
+    model_channel_factor: int
+    optimizer_learning_rate: float
+    optimizer_weight_decay: float
+    optimizer_type: str = "Adam"
+
+
 def train():
     logging.basicConfig(level="INFO")
     device = "cuda"
+
+    logger.info("Parsing config...")
+    [config_path] = sys.argv[1:]
+    with open(config_path) as f:
+        config_raw = yaml.safe_load(f)
+        config = TrainingConfig(**config_raw)
+    logger.info(f"Starting training with config:\n{pformat(config)}.")
+
     logger.info("Loading Data...")
     train_dataset = datasets.FashionMNIST(
         "data",
@@ -29,10 +50,11 @@ def train():
     )
 
     logger.info("Initializing model...")
-    model = ConvModel(image_channels=1, depth=3, num_classes=10)
+    model = ConvModel(image_channels=1, depth=3, num_classes=10, channel_factor=config.model_channel_factor)
     model = model.to(device)
 
-    optimizer = optim.Adam(model.parameters())
+    optimizer_cls = training.get_optimizer_class(config.optimizer_type)
+    optimizer = optimizer_cls(model.parameters(), lr=config.optimizer_learning_rate, weight_decay=config.optimizer_weight_decay)
     loss_fn = nn.CrossEntropyLoss()
     train_loader = DataLoader(train_dataset, batch_size=128, num_workers=8)
     validation_loader = DataLoader(validation_dataset, batch_size=256, num_workers=8)
@@ -44,6 +66,7 @@ def train():
         model=model,
         optimizer=optimizer,
         loss_fn=loss_fn,
+        epochs=config.epochs,
         device=device,
     )
     logger.info("Training finished.")
--- ml-experiments/project/src/image_classification/training.py
+++ ml-experiments/project/src/image_classification/training.py
@@ -8,15 +8,27 @@ from torch.utils.data import DataLoader
 logger = logging.getLogger(__name__)
 
 
+def get_optimizer_class(name: str):
+    match name:
+        case "Adam":
+            return optim.Adam
+        case "SGD":
+            return optim.SGD
+        case _:
+            raise ValueError(f"Unknown optimizer name {name}")
+
+
+
 def train(
     train_loader: DataLoader,
     validation_loader: DataLoader,
     model: nn.Module,
     loss_fn: nn.Module,
     optimizer: optim.Optimizer,
+    epochs: int,
     device: str,
 ):
-    for i in range(30):
+    for i in range(epochs):
         loss_mean = train_one_epoch(train_loader, model, loss_fn, optimizer, device)
         logger.info(f"Epoch {i + 1}, average training loss: {loss_mean:.2f}")
         if (i + 1) % 5 == 0:
